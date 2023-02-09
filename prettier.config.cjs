2d31d82f67a0b024d04cb4d6273e14a9e00e0f34
Felix Berger <felix.enok.berger@gmail.com>
2023-02-09 13:01:39 +0100

    About me section progress
    Still needs a bit of tweaking and a proper text the about me section

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,14 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-    plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  };
-  
+  plugins: [require.resolve('prettier-plugin-tailwindcss'), require.resolve('prettier-plugin-astro')],
+  semi: false,
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        nodejs: '#339933',
+        typescript: '#3178C6',
+        react: '#61dafb',
+
+        trpc: '#2596BE',
+        nextjs: '#000000',
+        headlessui: '#66E3FF',
+      }
+    },
+  },
+  plugins: [],
 }
