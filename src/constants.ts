3e1ea31da215be9cedeb48a532547bd5aef18e18
Felix Berger <felix.enok.berger@gmail.com>
2023-07-16 01:59:22 +0200

    Added constants & moved components

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const constants = {
+  job: {
+    title: 'Student Developer',
+    employer: 'Monsido',
+    employerUrl: 'https://monsido.com'
+  },
+  socials: [
+    {
+      title: 'Github',
+      url: 'https://github.com/dumspy',
+    },
+    {
+      title: 'Twitter',
+      url: 'https://twitter.com/_dumspy',
+    },
+    {
+      title: 'Linkedin',
+      url: 'https://linkedin.com/in/f-berger/',
+    },
+  ],
+}
