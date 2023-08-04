d8d7c2f2767a6d64469025e172bdae8810b9a1d9
Felix Berger <felix.enok.berger@gmail.com>
2023-08-04 23:12:39 +0200

    Design makeover (#1)
    * Updated packages
    * Fixed broken link and removed unused files
    * Added constants & moved components
    * Renamed and moved components, Added .editorconfig & more use of constants
    * Fixed linting to only lint src folder
    * Initial new design
    * Simple robots file

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const constants = {
+    fullname: 'Felix Berger',
+    job: {
+        title: 'Student Developer',
+        employer: 'Monsido',
+        employerUrl: 'https://monsido.com',
+    },
+    socials: [
+        {
+            title: 'Github',
+            url: 'https://github.com/dumspy',
+        },
+        {
+            title: 'Twitter',
+            url: 'https://twitter.com/_dumspy',
+        },
+        {
+            title: 'Linkedin',
+            url: 'https://linkedin.com/in/f-berger/',
+        },
+    ],
+}
