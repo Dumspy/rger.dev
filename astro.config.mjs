fc630dd452dab88718d60db04aa68715571b8cdb
Felix Berger <felix.enok.berger@gmail.com>
2023-12-27 18:23:31 +0100

    Upgraded astro related packages & small refactor(moving of things)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  experimental: {
-    assets: true
-  },
+  integrations: [tailwind(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
 });
