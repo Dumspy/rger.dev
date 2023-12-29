ebd6ac9d69b5b7456b581a610aef72ddacfdc2f8
Felix Berger <felix.enok.berger@gmail.com>
2023-12-29 16:24:01 +0100

    Added vercel speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercelStatic from '@astrojs/vercel/static';
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercelStatic from '@astrojs/vercel/static'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: "static",
-  adapter: vercelStatic({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
-    devImageService: 'sharp'
-  }),
-});
+    integrations: [tailwind(), icon()],
+    output: 'static',
+    adapter: vercelStatic({
+        webAnalytics: {
+            enabled: true,
+        },
+        speedInsights: {
+            enabled: true,
+        },
+        imageService: true,
+        devImageService: 'sharp',
+    }),
+})
